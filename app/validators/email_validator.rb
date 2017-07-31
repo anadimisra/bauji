b0b949bea0b277b4fb3f09ac9ac1b6d84b4eebaf
Anadi Misra <anadi.msr@gmail.com>
2017-07-31 17:57:18 +0530

    updated email validator to use devise like validation logic i.e. there should be only one @ symbol

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,7 +1,8 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@(?:[-a-z0-9\.]+)?[^\.\z]/i
+    unless value =~ /\A[^@\s]+@[^@\s]+\z/i
       record.errors[attribute] << (options[:message] || "is not an email")
     end
   end
 end
+
