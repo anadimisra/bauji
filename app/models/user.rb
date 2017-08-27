69d1d174b9e4c8efec0b4a87d96d2169c1fc42ed
Anadi Misra <anadi.msr@gmail.com>
2017-08-27 07:17:01 +0530

    users need approval from an admin user before they can use the system

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,21 @@ class User < ApplicationRecord
       end
   end  
 
+  def active_for_authentication? 
+    super && approved? 
+  end 
+  
+  def inactive_message 
+    if !approved? 
+      :not_approved 
+    else 
+      super # Use whatever other message 
+    end 
+  end
+  ## TODO
   # after_create :send_admin_mail
   # use active job with Resque backed by Redis 
-
+  #def send_admin_mail
+  #  AdminMailer.new_user_waiting_for_approval(self).deliver_later
+  #end
 end
